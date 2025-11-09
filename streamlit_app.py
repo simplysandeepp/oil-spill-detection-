eb5aad8f998269328c0210d93c0b81ada7f6fe15
Sandeep prajapati <sandeepprajapati1202@gmail.com>
2025-11-09 12:56:42 +0530

    final update!

--- streamlit_app.py
+++ streamlit_app.py
@@ -612,7 +612,10 @@ def main():
         <p class="subtitle">
             Our system leverages cutting-edge Deep Learning and AI technologies to detect and analyze oil spills from satellite and aerial imagery with high speed and accuracy. Designed for environmental monitoring agencies, researchers, and response teams, it transforms raw imagery into actionable insights, helping protect marine ecosystems and coastal communities.
         </p>
-        <p class="author"><a href="https://www.linkedin.com/in/simplysandeepp/" target="_blank" style="color: inherit; text-decoration: none;"> Developed by Sandeep Prajapati ♥️</a></p>
+        <p class="author"><a href="https://www.linkedin.com/in/simplysandeepp/" target="_blank" style="color: inherit; text-decoration: none;"> Developed by Sandeep Prajapati</a></p>
+        <p class="author"><a href="https://www.linkedin.com/in/rawatkhushi/" target="_blank" style="color: inherit; text-decoration: none;"> Developed by Khushi Rawatti</a></p>
+        <p class="author"><a href="https://www.linkedin.com/in/siya-kumari-7315b4354/" target="_blank" style="color: inherit; text-decoration: none;"> Developed by Siya Kumari</a></p>
+        <p class="author"><a href="https://www.linkedin.com/in/chhabravansh/" target="_blank" style="color: inherit; text-decoration: none;"> Developed by Vansh Chhabra</a></p>
 
     </div>
     """, unsafe_allow_html=True)
@@ -1245,7 +1248,7 @@ def main():
             Made under GDG Noida Build-a-thon 🌟
         </p>
         <p style="margin-top: 10px; font-size: 0.95rem; opacity: 0.9;">
-            Powered by Deep Learning • Streamlit • Supabase
+            Powered by • Deep Learning • Streamlit • Supabase
         </p>
         <p style="margin-top: 15px; font-size: 0.85rem; opacity: 0.8;">
             © 2025 HydroVexel. All rights reserved. | For environmental monitoring and research purposes.
